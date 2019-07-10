7b14ec7069ec0faab4e040caea2c2a94ec3f1103
Amanda Phillipson <amanda.minton@gmail.com>
2019-07-10 09:34:09 -0400

    Do not have working JS code. Was able to format the calculator layout using css grid.

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// Tried a few different things, but unable to produce any working JS Code.
+// Code below is just a placeholder as I work it out. 
+
+
+// const numberClick = document.querySelector('')
+// numberClick.addEventListener('click', function () {
+//   numberClick.clickquerySelector('input')
+// })
+
+
+// const CalculatorButtons = document.querySelector('button');
+
+// let CalculatorButtons = document.getElementById("CalculatorButtons");
+//     CalculatorButtons.addEventListener('mouseover', event => {
+//     button.innerHTML = 
+
+//     `Click count: ${event.detail}`;
+// });
