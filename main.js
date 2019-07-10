93d5237c1695e8ab9fab39d58ebad327c07dcdf7
Amanda Phillipson <amanda.minton@gmail.com>
2019-07-10 16:26:46 -0400

    Added JS to display user input by clicking the buttons in the calculator. No mathmatics involved, yet.

--- main.js
+++ main.js
@@ -1,19 +1,89 @@
+let display = document.querySelector('.input') 
 
-// Tried a few different things, but unable to produce any working JS Code.
-// Code below is just a placeholder as I work it out. 
+document.querySelector('.clear').addEventListener('click', function (event){
+    display.innerText=""
+    })
 
+document.querySelector('.input').addEventListener('click', function (event){
+    display.innerHTML=""
+    })
 
-// const numberClick = document.querySelector('')
-// numberClick.addEventListener('click', function () {
-//   numberClick.clickquerySelector('input')
-// })
+let seven = document.querySelector('.seven')
+    seven.addEventListener('click', function (event){
+        display.innerText+=seven.innerText
+    })
 
+let eight = document.querySelector('.eight')
+    eight.addEventListener('click', function (event){
+        display.innerText+=eight.innerText
+    })
 
-// const CalculatorButtons = document.querySelector('button');
+let nine = document.querySelector('.nine')
+    nine.addEventListener('click', function (event){
+        display.innerText+=nine.innerText
+    })
 
-// let CalculatorButtons = document.getElementById("CalculatorButtons");
-//     CalculatorButtons.addEventListener('mouseover', event => {
-//     button.innerHTML = 
+let divide = document.querySelector('.divide')
+    divide.addEventListener('click', function (event){
+        display.innerText+=divide.innerText
+    })
 
-//     `Click count: ${event.detail}`;
-// });
+let four = document.querySelector('.four')
+    four.addEventListener('click', function (event){
+        display.innerText+=four.innerText
+    })
+
+let five = document.querySelector('.five')
+    five.addEventListener('click', function (event){
+        display.innerText+=five.innerText
+    })
+
+let six = document.querySelector('.six')
+    six.addEventListener('click', function (event){
+        display.innerText+=six.innerText
+    })
+
+let multiply = document.querySelector('.multiply')
+    multiply.addEventListener('click', function (event){
+        display.innerText+=multiply.innerText
+    })
+
+let one = document.querySelector('.one') 
+    one.addEventListener('click', function (event){
+    display.innerText+=one.innerText
+    })
+
+let two = document.querySelector('.two')
+    two.addEventListener('click', function (event){
+    display.innerText+=two.innerText 
+    })
+
+let three = document.querySelector('.three')
+    three.addEventListener('click', function (event){
+    display.innerText+=three.innerText
+    })
+
+let subtract = document.querySelector('.subtract')
+    subtract.addEventListener('click', function (event){
+    display.innerText+=subtract.innerText
+    })
+
+let zero = document.querySelector('.zero')
+    zero.addEventListener('click', function (event){
+    display.innerText+=zero.innerText
+    })
+
+let period = document.querySelector('.period')
+    period.addEventListener('click', function (event){
+        display.innerText+=period.innerText
+    })
+
+let equal = document.querySelector('.equal')
+    equal.addEventListener('click', function (event){
+        display.innerText+=equal.innerText
+    })
+
+let add = document.querySelector('.add')
+    add.addEventListener('click', function (event){
+        display.innerText+=add.innerText
+    })
